7427524a5a83c3c3b122d4b28fee060fb4db5089
Florent <florent.guyard@live.fr>
2024-05-13 11:47:45 +0200

    intégration de la modale provenant du module npm

---
+++ HRnet/src/Components/Form/DepartmentData.jsx
@@ -0,0 +1,34 @@
+
+
+const options = [
+    {
+        value: 1,
+        label: 'Sales'
+      },
+      {
+        value: 2,
+        label: 'Marketing'
+      },
+      {
+        value: 3,
+        label: 'Engineering'
+      },
+      {
+        value: 4,
+        label: 'Human Resources'
+      },
+      {
+        value: 5,
+        label: 'Legal'
+      },
+];
+
+const optionsDepartment = options.map((options, value) => ({
+    value: value,
+    label: options.label,
+  }));
+  
+
+  
+
+  export default optionsDepartment;
--- HRnet/src/Components/Form/Form.jsx
+++ HRnet/src/Components/Form/Form.jsx
@@ -5,71 +5,92 @@ import 'react-calendar/dist/Calendar.css';
 import { useState, useEffect } from "react";
 import Select from "react-dropdown-select";
 import  optionsState from "./StateData.jsx";
+import optionsDepartment from "./DepartmentData.jsx";
+import Modal_module from "modal_florent_guyard";
 
 
 const Form = () => {
 
-    console.log(optionsState)
-
-    const options = [
-        {
-            value: 1,
-            label: 'Sales'
-          },
-          {
-            value: 2,
-            label: 'Marketing'
-          },
-          {
-            value: 3,
-            label: 'Engineering'
-          },
-          {
-            value: 4,
-            label: 'Human Resources'
-          },
-          {
-            value: 5,
-            label: 'Legal'
-          },
-    ];
-
-    /*const options = [
-        {
-          value: 1,
-          label: 'Leanne Graham'
-        },
-        {
-          value: 2,
-          label: 'Ervin Howell'
-        }
-      ];*/
+    
 
     const [ birthDate, setBirthDate ] = useState(new Date());
     const [ startDate, setStartDate ] = useState(new Date());
+    const [isModaleVisible, setIsModaleVisible] = useState(false);
+    
 
+    useEffect(() => {
+
+        const searchModal = document.querySelector('.modal');
+        console.log(searchModal);
+
+        if(searchModal) {
+
+            const searchValidation = document.querySelector('.modal_div_cross');
+            searchValidation.addEventListener('click', () => {
+                setIsModaleVisible(false);
+            });
+
+            const searchModal = document.querySelector('.modal:not(.modal_div)');
+            searchModal.addEventListener('click', (event) => {
+                if (event.target === searchModal) {
+                    setIsModaleVisible(false);
+                }
+            });
+            
+        } 
+    
+    },[isModaleVisible])
 
     const handleChange = (event) => {
         setBirthDate(event);
+        console.log(birthDate)
     };
 
     const handleChangeStart = (event) => {
         setStartDate(event);
     };
 
-    /*const handleChangeDepartment = (event) => {
-        console.log(event)
-    }*/
 
-    useEffect(() => {
-        setBirthDate();
-        setStartDate();
-    }, [])
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        let stateValue;
+        let departmentValue;
+
+        const firstName = document.getElementById('first_name').value;
+        const lastName = document.getElementById('last_name').value;
+        const street = document.getElementById('street').value;
+        const city = document.getElementById('city').value;
+        const state = document.querySelector(".state");
+        if (state) {
+             stateValue = state.querySelector(".react-dropdown-select-content").textContent;
+        }
+        const zipCode = document.getElementById('zip_code').value;
+        const department = document.querySelector(".department");
+        if (department) {
+            departmentValue = department.querySelector(".react-dropdown-select-content").textContent;
+        }
+
+
+        /*console.log("firstname : ", firstName);
+        console.log("lastName : ", lastName);
+        console.log("birth :", birthDate);
+        console.log("start :", startDate);
+        console.log("state :", stateValue);
+        console.log("street : ", street);
+        console.log("city : ", city);
+        console.log("zipCode : ", zipCode);
+        console.log("department :", departmentValue);*/
+
+        setIsModaleVisible(true);
+
+    }
+
+
 
     return (
 
         <section className="section_form">
-            <form className="form">
+            <form className="form" onSubmit={(event) => handleSubmit(event)}>
 
                 <div className="form_div">
                     <label htmlFor="first_name">First Name</label>
@@ -83,7 +104,7 @@ const Form = () => {
 
                 <div className="form_div">
                     <label htmlFor="birth">Date of Birth</label>
-                    <DatePicker onChange={(event) => handleChange(event)} value={birthDate}/>
+                    <DatePicker onChange={(event) => handleChange(event)} value={birthDate} id="birth"/>
                 </div>
 
                 <div className="form_div">
@@ -96,33 +117,39 @@ const Form = () => {
 
                     <div className="form_div">
                         <label>Street</label>
-                        <input type="text"></input>
+                        <input type="text" id="street"></input>
                     </div>
                     
                     <div className="form_div">
                         <label>City</label>
-                        <input type="text"></input>
+                        <input type="text" id="city"></input>
                     </div>
 
                     <div className="form_div">
                         <label>State</label>
-                       <Select options={optionsState} />
+                        <span className="state"><Select options={optionsState}  /></span>
+                       
                     </div>
 
                     <div className="form_div">
                         <label>Zip Code</label>
-                        <input type="text"></input>
+                        <input type="text" id="zip_code"></input>
                     </div>
 
                 </fieldset>
 
-                    <div className="form_fieldset_state">
+                    <div className="form_department">
                         <label>Department</label>
-                        <Select options={options}  />
+                        <span className="department">
+                            <Select options={optionsDepartment}  />
+                        </span>
                     </div>
 
-                <Button />
+                <Button className= "form_button" />
             </form>
+                {isModaleVisible ? <Modal_module /> : ""}
+                
+
         </section>
     )
 }
